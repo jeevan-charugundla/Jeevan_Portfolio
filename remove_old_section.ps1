de1a8960add31a25009d8110f29ce2d1f27c8393
jeevan-charugundla <jeevan.charu06@gmail.com>
2026-04-19 00:37:34 +0530

    Initialize NeoBrutalist portfolio: clean production state

--- remove_old_section.ps1
+++
@@ -1,4 +0,0 @@
-$lines = Get-Content 'index.html'
-$keep = $lines[0..1536] + $lines[1742..($lines.Length-1)]
-Set-Content 'index.html' -Value $keep -Encoding UTF8
-Write-Host "Done. Lines remaining: $($keep.Length)"
